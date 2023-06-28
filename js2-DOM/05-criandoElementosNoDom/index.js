ea8ec0c5aae290fad9206c08417d101c7a6febc4
marcosDev2 <dev2@aproms.com.br>
2023-06-28 12:36:46 -0400

    inicio do dom

---
+++ js2-DOM/05-criandoElementosNoDom/index.js
@@ -0,0 +1,14 @@
+function addInput(){
+    const ul = document.getElementById('inputs')
+
+    const newLI = document.createElement('li')
+    newLI.className = 'list-item'
+    newLI.innerHTML = 'Novo input: '
+
+    const newInput = document.createElement('input')
+    newInput.type = 'text'
+    newInput.name = 'input'
+
+    newLI.appendChild(newInput)
+    ul.appendChild(newLI)
+}
